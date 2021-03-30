03f0195ca7521719fd7e195e10d0436a6730c38e
booyahboy <theflyingabers@outlook.com>
2021-03-29 21:34:47 -0400

    Added csvToTable and ClashofClans CSV data

---
+++ site.js
@@ -0,0 +1,25 @@
+function csvToTable(tableSelector, csvPath) {
+  const thead = $(tableSelector + " > thead")[0];
+  const tbody = $(tableSelector + " > tbody")[0];
+  jQuery.get({
+    url: csvPath,
+    success: function (response) {
+      const data = jQuery.csv.toArrays(response);
+      for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
+        const cellArray = data[rowIndex];
+        const tableRow = document.createElement("tr");
+
+        for (let colIndex = 0; colIndex < cellArray.length; colIndex++) {
+          const tableCell = document.createElement(
+            rowIndex === 0 ? "th" : "td"
+          );
+          tableCell.textContent = cellArray[colIndex];
+          tableRow.appendChild(tableCell);
+        }
+
+        if (rowIndex === 0) thead.appendChild(tableRow);
+        else tbody.appendChild(tableRow);
+      }
+    },
+  });
+}
